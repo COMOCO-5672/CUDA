e77ad469b06b7ee37c936a3c04836cfcee5d238f
comoco-5672 <775324623@qq.com>
2023-06-24 11:59:23 +0800

    add pycuda

---
+++ PyCUDA/FirstPyCUDA/main.py
@@ -0,0 +1,21 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import pycuda.driver as drv
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    drv.init()
+    print(drv.Device.count())
+    print(drv.Device(0).name())
+    print(drv.Device(0).compute_capability())
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
