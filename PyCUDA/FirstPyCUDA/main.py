b56be41aa3c8f2b67a74d83b98869b822b872f0f
培伦 焦 <775324623@qq.com>
2024-09-07 18:02:10 +0800

    更新python文件

--- PyCUDA/FirstPyCUDA/main.py
+++ PyCUDA/FirstPyCUDA/main.py
@@ -1,21 +1,29 @@
-# This is a sample Python script.
+import numpy as np
+import os
+import pycuda.autoinit
+from pycuda import gpuarray
+from time import time
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+custom_flags = "-allow-unsupported-compiler"
+os.environ["PYCUDA_DEFAULT_NVCC_FLAGS"] = custom_flags
 
-import pycuda.driver as drv
+host_data = np.float32( np.random.random(50000000) )
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    drv.init()
-    print(drv.Device.count())
-    print(drv.Device(0).name())
-    print(drv.Device(0).compute_capability())
+t1 = time()
+host_data_2x =  host_data * np.float32(2)
+t2 = time()
+
+print ('total time to compute on CPU: %f' % (t2 - t1))
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+device_data = gpuarray.to_gpu(host_data)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+t1 = time()
+device_data_2x =  device_data * np.float32( 2 )
+t2 = time()
+
+from_device = device_data_2x.get()
+
+
+print ('total time to compute on GPU: %f' % (t2 - t1))
+print ('Is the host computation the same as the GPU computation? : {}'.format(np.allclose(from_device, host_data_2x) ))
